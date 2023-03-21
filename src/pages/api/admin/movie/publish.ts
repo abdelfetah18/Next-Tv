7fafc75a156af6ac4af281b5712ef655b1d79f1e
abdelfetah18 <nstart0319@gmail.com>
2023-03-21 19:44:52 +0100

    Make save document and publish document using a seperate api and fixing some issues

---
+++ src/pages/api/admin/movie/publish.ts
@@ -0,0 +1,16 @@
+import client from '@/database/connection';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method == "POST"){
+        let movie = req.body;
+        client.publishMovie(movie).then((result) => {
+            res.status(200).json({ status: "success", data: result });
+        }).catch(err => {
+            res.status(200).json({ status: "error", message: "something went wrong!", error: err });
+        });
+    }else{
+        res.status(200).json({ status: "not_found", message: "Method not found!" });
+    }
+}
---
+++ src/pages/api/admin/serie/update.ts
@@ -0,0 +1,17 @@
+import client from '@/database/connection';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method == "POST"){
+        let serie = req.body;
+        
+        client.updateSerie(serie).then((result) => {
+            res.status(200).json({ status: "success", data: result });
+        }).catch(err => {
+            res.status(200).json({ status: "error", message: "something went wrong!", error: err });
+        });
+    }else{
+        res.status(200).json({ status: "not_found", message: "Method not found!" });
+    }
+}
